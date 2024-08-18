2d417312a7c69cb214eb08d67bf46aa772f5b824
hareesh-dot <ayyapagari.hareesh@gmail.com>
2024-08-18 11:54:20 +0530

    operators examples

---
+++ Membership operators.py
@@ -0,0 +1,5 @@
+# Membership operators --- return true if the value present in the sequesnce
+
+t = (1,2,3,1,2,3)
+print('T',3 in t)
+print('F',6 in t)
---
+++ logical operators.py
@@ -0,0 +1,19 @@
+# logical operators
+
+# and  or     not 
+a,b = True,False
+
+# and
+result_and = a and b
+print(result_and)
+
+result_and = a and True
+print(result_and)
+
+# or
+result_or = a or b
+print(result_and)
+
+# not
+result_not = not a
+print(result_not)
